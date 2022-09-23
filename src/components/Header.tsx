e26fbca55a5bc78dad30c6f49f84a99d70bc124f
michalm08 <michalski@gmail.com>
2022-09-23 15:58:30 +0200

    feat(front): create header and add colors variables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import './Header.scss'
+import './Header.scss';
+//@ts-ignore
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <>
-      <div className='header'>Header</div>
+      <div className='header-container'>
+        <div className='header'>
+          <img src={logo} alt='photo' />
+          <input className='header__search' type='text' placeholder='Search' />
+        </div>
+      </div>
       <Outlet />
     </>
   );
